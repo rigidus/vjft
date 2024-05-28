52cfe0a5a1e57bd4a9dee145f025f9fc28ea1571
Rigidus <i.am.rigidus@gmail.com>
2024-05-28 14:13:23 +0300

    remove virtual draw()-methods from SceneObject class

--- EventListener.hpp
+++ EventListener.hpp
@@ -7,6 +7,4 @@ class EventListener {
 public:
     virtual ~EventListener() = default;
     virtual void onEvent(const Event& event) = 0;
-    virtual void draw(SDL_Renderer* renderer, std::shared_ptr<Viewport> viewport) = 0;
-    virtual SDL_Rect getBoundingBox() const = 0;
 };
