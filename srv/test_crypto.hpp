86ef82f7c3a555ac55c5638d34c41f64ff430eb6
Rigidus <i.am.rigidus@gmail.com>
2024-06-09 04:16:20 +0300

    missed test_crypto.hpp

---
+++ srv/test_crypto.hpp
@@ -0,0 +1,14 @@
+// test_crypto.hpp
+#ifndef TEST_CRYPTO_HPP
+#define TEST_CRYPTO_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <openssl/evp.h>
+#include "Client.hpp"
+
+// Функция для тестирования полной последовательности шифрования, кодирования, подписывания и обратного процесса
+bool TestFullSequence(EVP_PKEY* private_key, EVP_PKEY* public_key);
+
+#endif // TEST_CRYPTO_HPP
