201a7f93fbc478a2bd3eb4b0b63722301a86904a
Rigidus <i.am.rigidus@gmail.com>
2024-07-18 02:40:52 +0300

    term: key_map: modifyers for F-keys

--- term/key_map.h
+++ term/key_map.h
@@ -52,6 +52,7 @@ Ctrl-V перед специальной клавишей заставит cat �
 // Список клавиатурных входов
 #define KEY_MAP(X)                              \
     X(KEY_UNKNOWN, "")                          \
+    X(KEY_ESC, "\x1b")                          \
     X(KEY_TAB,       "\x09")                    \
     X(KEY_SHIFT_TAB, "\x1b[Z")                  \
     X(KEY_BACKSPACE,          "\x7f")           \
@@ -88,13 +89,68 @@ Ctrl-V перед специальной клавишей заставит cat �
     X(KEY_CTRL_F1,       "\x1b[1;5P")           \
     X(KEY_SHIFT_CTRL_F1, "\x1b[1;6P")           \
     X(KEY_F2, "\x1bOQ")                         \
+    X(KEY_SHIFT_F2,      "\x1b[1;2Q")           \
+    X(KEY_ALT_F2,        "\x1b[1;3Q")           \
+    X(KEY_SHIFT_ALT_F2,  "\x1b[1;4Q")           \
+    X(KEY_CTRL_F2,       "\x1b[1;5Q")           \
+    X(KEY_SHIFT_CTRL_F2, "\x1b[1;6Q")           \
     X(KEY_F3, "\x1bOR")                         \
+    X(KEY_SHIFT_F3,      "\x1b[1;2R")           \
+    X(KEY_ALT_F3,        "\x1b[1;3R")           \
+    X(KEY_SHIFT_ALT_F3,  "\x1b[1;4R")           \
+    X(KEY_CTRL_F3,       "\x1b[1;5R")           \
+    X(KEY_SHIFT_CTRL_F3, "\x1b[1;6R")           \
     X(KEY_F4, "\x1bOS")                         \
+    X(KEY_SHIFT_F4,      "\x1b[1;2S")           \
+    X(KEY_ALT_F4,        "\x1b[1;3S")           \
+    X(KEY_SHIFT_ALT_F4,  "\x1b[1;4S")           \
+    X(KEY_CTRL_F4,       "\x1b[1;5S")           \
+    X(KEY_SHIFT_CTRL_F4, "\x1b[1;6S")           \
     X(KEY_F5, "\x1b[15~")                       \
+    X(KEY_SHIFT_F5,      "\x1b[15;2~")          \
+    X(KEY_ALT_F5,        "\x1b[15;3~")          \
+    X(KEY_SHIFT_ALT_F5,  "\x1b[15;4~")          \
+    X(KEY_CTRL_F5,       "\x1b[15;5~")          \
+    X(KEY_SHIFT_CTRL_F5, "\x1b[15;6~")          \
     X(KEY_F6, "\x1b[17~")                       \
+    X(KEY_SHIFT_F6,      "\x1b[17;2~")          \
+    X(KEY_ALT_F6,        "\x1b[17;3~")          \
+    X(KEY_SHIFT_ALT_F6,  "\x1b[17;4~")          \
+    X(KEY_CTRL_F6,       "\x1b[17;5~")          \
+    X(KEY_SHIFT_CTRL_F6, "\x1b[17;6~")          \
     X(KEY_F7, "\x1b[18~")                       \
+    X(KEY_SHIFT_F7,      "\x1b[18;2~")          \
+    X(KEY_ALT_F7,        "\x1b[18;3~")          \
+    X(KEY_SHIFT_ALT_F7,  "\x1b[18;4~")          \
+    X(KEY_CTRL_F7,       "\x1b[18;5~")          \
+    X(KEY_SHIFT_CTRL_F7, "\x1b[18;6~")          \
     X(KEY_F8, "\x1b[19~")                       \
+    X(KEY_SHIFT_F8,      "\x1b[19;2~")          \
+    X(KEY_ALT_F8,        "\x1b[19;3~")          \
+    X(KEY_SHIFT_ALT_F8,  "\x1b[19;4~")          \
+    X(KEY_CTRL_F8,       "\x1b[19;5~")          \
+    X(KEY_SHIFT_CTRL_F8, "\x1b[19;6~")          \
     X(KEY_F9, "\x1b[20~")                       \
+    X(KEY_SHIFT_F9,      "\x1b[20;2~")          \
+    X(KEY_ALT_F9,        "\x1b[20;3~")          \
+    X(KEY_SHIFT_ALT_F9,  "\x1b[20;4~")          \
+    X(KEY_CTRL_F9,       "\x1b[20;5~")          \
+    X(KEY_SHIFT_CTRL_F9, "\x1b[20;6~")          \
     X(KEY_F10, "\x1b[21~")                      \
+    X(KEY_SHIFT_F10,      "\x1b[21;2~")         \
+    X(KEY_ALT_F10,        "\x1b[21;3~")         \
+    X(KEY_SHIFT_ALT_F10,  "\x1b[21;4~")         \
+    X(KEY_CTRL_F10,       "\x1b[21;5~")         \
+    X(KEY_SHIFT_CTRL_F10, "\x1b[21;6~")         \
     X(KEY_F11, "\x1b[23~")                      \
-    X(KEY_F12, "\x1b[24~")
+    X(KEY_SHIFT_F11,      "\x1b[23;2~")         \
+    X(KEY_ALT_F11,        "\x1b[23;3~")         \
+    X(KEY_SHIFT_ALT_F11,  "\x1b[23;4~")         \
+    X(KEY_CTRL_F11,       "\x1b[23;5~")         \
+    X(KEY_SHIFT_CTRL_F11, "\x1b[23;6~")         \
+    X(KEY_F12, "\x1b[24~")                      \
+    X(KEY_SHIFT_F12,      "\x1b[24;2~")         \
+    X(KEY_ALT_F12,        "\x1b[24;3~")         \
+    X(KEY_SHIFT_ALT_F12,  "\x1b[24;4~")         \
+    X(KEY_CTRL_F12,       "\x1b[24;5~")         \
+    X(KEY_SHIFT_CTRL_F12, "\x1b[24;6~")
